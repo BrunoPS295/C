57e2e0f60a7ad1c83d3131fc251a3d5001d0b5d7
Bruno Pacheco <148828825+BrunoPS295@users.noreply.github.com>
2025-05-20 19:23:09 -0300

    +base C, ex1 e ex2

---
+++ Exercicios/ex1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    // comentario em linha
+    /*
+        Comentario em bloco
+    */
+
+    /*
+        \n é o comando para pular uma linha em um print
+        \n é uma função de saida
+    */
+    printf("Hello world\nOla mundo");
+    
+    /*
+        %d retorna a quantidade de cacteres do printf seguinte
+    */
+   printf("Valor: %d", printf("\ncarambolas\n"));
+   
+   return 0;
+}
---
+++ Exercicios/ex2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("\nPressione qualque tecla para sair: ");
+    //getchar serve para o terminal receber uma tecla e só assim continuar a executar o código
+    getchar();
+    printf("\nSLA");
+    return 0;
+}
